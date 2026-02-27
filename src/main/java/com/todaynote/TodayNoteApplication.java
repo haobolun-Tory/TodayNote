8192959a0f99847b4f161a3e2842dd7f2459c71c
haobolun-Tory <haobolun@foxmail.com>
2026-02-27 22:23:57 +0800

    Initial commit

---
+++ src/main/java/com/todaynote/TodayNoteApplication.java
@@ -0,0 +1,13 @@
+package com.todaynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todaynote/TodayNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
